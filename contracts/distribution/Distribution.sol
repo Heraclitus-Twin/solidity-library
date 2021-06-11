4f30fe6f1d0fb369b181d5ae462fab6b70652cbb
Ἡράκλειτος <84381664+Heraclitus-Twin@users.noreply.github.com>
2021-06-11 15:32:44 +0800

    remove unused dependency

--- contracts/distribution/Distribution.sol
+++ contracts/distribution/Distribution.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.0;
 
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {SafeERC20, IERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import {EnumerableSet} from '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
 
 contract Distribution is Ownable {
     using SafeERC20 for IERC20;
-    using EnumerableSet for EnumerableSet.UintSet;
 
     uint public constant MULTIPLIER = 1e36;
 
